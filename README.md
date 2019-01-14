f8e7f8b54babc8337fda2ad0f4e2c1665d021e0f
korabelnikov <nesoriti@yandex.ru>
2019-01-14 15:53:37 +0300

    Clean-up. some lines in html/flask have been commented. mock added for test without ML engine.

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # vkfaces_web
 TODO
-
-ML:
-face detection via mtcnn in https://github.com/davidsandberg/facenet/tree/master/src/align
--- webform.py
+++ webform.py
@@ -1,10 +1,8 @@
 import os
 from flask import Flask, render_template, request, send_from_directory, url_for
-from os import path
-import glob
 import shutil
 
-from reidentifier import Reidentifier
+from face import classify
 
 app = Flask(__name__)
 
@@ -17,62 +15,33 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-class_names = ['1', '2']
-
 shutil.rmtree(UPLOAD_FOLDER)
-for class_name in class_names:
-    os.makedirs(path.join(UPLOAD_FOLDER, class_name))
-
-
-filenames = {'1': [], '2': []}  # [string, list[string]]
-
-reidentifier = Reidentifier()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', image_files=filenames, class_names=class_names)
+    #todo
+    return render_template('index.html')
 
 
-@app.route('/upload_train', methods=['POST'])
-def upload_train():
-    class_n = request.form['class']
-    file = request.files['image']
-    if file:
-        f = os.path.join(app.config['UPLOAD_FOLDER'], class_n, file.filename)
-        file.save(f)
-        filename = url_for('static', filename='uploads/' + class_n + '/' + os.path.basename(f))
-        filenames[class_n].append(filename)
-
-    return render_template('index.html', image_files=filenames, class_names=class_names)
-
-
-@app.route('/upload_test', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_test():
     file = request.files['image']
     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     if file:
-        if len(filenames[class_names[0]]) > 0 and len(filenames[class_names[1]]) > 0:
             file.save(f)
             filename = url_for('static', filename='uploads/' + os.path.basename(f))
+            k = 20
+            ids, conf = classify(filename, k)
+            return render_template('index.html', image_files=filename, ids=ids, confidence=conf)
 
-            class_name_test, score = reidentifier.reindetify(testImagePath=f, train_image_dir=UPLOAD_FOLDER)
-            return render_template('index.html', image_files=filenames, class_names=class_names,
-                                   test_image=filename, class_test=class_name_test, confidence=score)
-
-    return render_template('index.html', image_files=filenames, class_names=class_names)
+    return render_template('index.html')
 
 
 @app.route('/clear', methods=['POST'])
 def clear():
-    class_n = request.form['class']
-    filenames[class_n] = []
-
-    files = glob.glob(path.join(UPLOAD_FOLDER, class_n, '*'))
-    for f in files:
-        os.remove(f)
-
-    return render_template('index.html', image_files=filenames, class_names=class_names)
+    #todo
+    return render_template('index.html')
 
 
 @app.route('/static/uploads/<filename>')
@@ -82,4 +51,7 @@ def send_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    local = '127.0.0.1'  # to run on local machine
+    internet = '0.0.0.0'  # to run on server and to be accessed outside
+
+    app.run(host=local, port=8081, debug=False)
